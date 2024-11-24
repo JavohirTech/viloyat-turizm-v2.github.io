df7a1cd589645319b5760b50adcab19c126ff370
javohirbro <javohir@felix-its.uz>
2024-11-24 19:47:05 +0500

    TASK / fixed LocaleLayout problem

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,34 +1,29 @@
-import {getMessages, getTranslations} from 'next-intl/server';
-import React, {ReactNode} from 'react';
-import {notFound} from "next/navigation";
-import {routing} from "@/i18n/routing";
+import { getMessages, getTranslations } from 'next-intl/server';
+import React from 'react';
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
 import RootLayout from '../layout';
 
-type LocaleType = "uz" | "ru" | "en";
+type LayoutProps = {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+};
 
-export async function generateMetadata({params}: { params: { locale: LocaleType } }) {
-  const t = await getTranslations({locale: params.locale});
-  return {
-    title: t('home')
-  };
-}
-
-type Props = {
-  children: ReactNode;
-  params: { locale: string };
-}
-
-export default async function LocaleLayout(props: Props) {
-  const locale = props.params.locale as LocaleType;
+export default async function LocaleLayout({
+                                             children,
+                                             params,
+                                           }: LayoutProps) {
+  const { locale } = await params;
   const messages = await getMessages();
 
-  if (!routing.locales.includes(locale)) {
+  // Check if the locale is valid
+  if (!routing.locales.includes(locale as "uz" | "ru" | "en")) {
     notFound();
   }
 
   return (
       <RootLayout locale={locale} messages={messages}>
-        {props.children}
+        {children}
       </RootLayout>
   );
 }
